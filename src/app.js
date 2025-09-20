ffb0b11bfa028b8af0521266147084c407c66902
Kawaljeet Singh Bharaj <kawaljeetsinghbharaj.jsb@gmail.com>
2025-09-21 01:08:27 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const helmet = require('helmet');
 const routes = require('./routes');
+const { errorHandler } = require('./middleware/errorHandler');
+
+const app = express();
+
+app.use(helmet());
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/api', routes);
+
+app.get('/', (req, res) => res.json({ ok: true, message: 'Water Management API' }));
+
+app.use(errorHandler);
+
+// This is the most important line for this fix
+module.exports = app;
